35879c85a1b5be3b72d43c4ff9a9c1a9dd1d2989
Aviral Asthana <aviral.asthana0704@gmail.com>
2025-01-24 14:55:38 +0530

    Initialised files and setup Hono

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,12 @@
+import { Hono } from "hono";
+import { handle } from "hono/cloudflare-pages";
+
+export const runtime  = "edge"; //Cloudflare Workers is the under the hood runtime of Vercel also so deploying there would be beneficial for performance
+const app =  new Hono().basePath("/api") //now no need to worry about the base path of the app for every single route we are defining it here
+
+app.get("/search", (c) => {
+    return c.json({})
+})
+
+export const GET = handle(app); // this handle function is integration function of Vercel with Hono so we can also deploy it to Vercel and make it compatible like Next.js
+export default app as never; // this is just to make sure that the app is not exported as a default export as nextjs compiler does not like it to be exported as default export just to bypass it
