8f4cb5323a1c34b4c83931b9523c63d43b972456
Aviral Asthana <aviral.asthana0704@gmail.com>
2025-01-24 15:22:24 +0530

    Update Hono integration for Vercel and add CORS support

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,12 +1,13 @@
 import { Hono } from "hono";
-import { handle } from "hono/cloudflare-pages";
-
+import { handle } from "hono/vercel";
+import { cors } from "hono/cors";
 export const runtime  = "edge"; //Cloudflare Workers is the under the hood runtime of Vercel also so deploying there would be beneficial for performance
 const app =  new Hono().basePath("/api") //now no need to worry about the base path of the app for every single route we are defining it here
-
+app.use('/*', cors())
 app.get("/search", (c) => {
     return c.json({})
 })
 
 export const GET = handle(app); // this handle function is integration function of Vercel with Hono so we can also deploy it to Vercel and make it compatible like Next.js
-export default app as never; // this is just to make sure that the app is not exported as a default export as nextjs compiler does not like it to be exported as default export just to bypass it
+export const POST = handle(app);
+export default app as never // this is just to make sure that the app is not exported as a default export as nextjs compiler does not like it to be exported as default export just to bypass it
